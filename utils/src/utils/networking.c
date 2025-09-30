91eb6fb9f60e11ea79aa41bef533550ade15c4cd
Elias Aires <eaires@frba.utn.edu.ar>
2025-09-30 11:54:34 -0300

    cambios

--- utils/src/utils/networking.c
+++ utils/src/utils/networking.c
@@ -94,20 +94,20 @@ int connect_to_server(char *IP, char *PORT, t_log *log)
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
 
-    if ((return_value = getaddrinfo(IP, PORT, &hints, &server_info)) != 0) 
+    if ((return_value = getaddrinfo(IP, PORT, &hints, &server_info)) != 0)
     {
         log_error(log, "getaddrinfo %s", gai_strerror(return_value));
         exit(-1);
     }
 
-    for (pointer = server_info; pointer != NULL; pointer = pointer->ai_next) 
+    for (pointer = server_info; pointer != NULL; pointer = pointer->ai_next)
     {
-        if ((socket_client = socket(pointer->ai_family, pointer->ai_socktype, pointer->ai_protocol)) == -1) 
+        if ((socket_client = socket(pointer->ai_family, pointer->ai_socktype, pointer->ai_protocol)) == -1)
         {
             log_warning(log, "client: socket %s, intentando nuevamente", strerror(errno));
             continue;
         }
-        
+
         if ((return_value = connect(socket_client, pointer->ai_addr, pointer->ai_addrlen)) == -1)
         {
             log_warning(log, "client: connect %s, intentando nuevamente", strerror(errno));
@@ -131,14 +131,16 @@ int connect_to_server(char *IP, char *PORT, t_log *log)
     return socket_client;
 }
 
-void enviar_resultado_handshake(int fd_conexion, resultado_t resultado, t_log* logger) {
+void enviar_resultado_handshake(int fd_conexion, resultado_t resultado, t_log *logger)
+{
     int size = sizeof(op_code) + sizeof(resultado_t);
-    void* stream = malloc(size);
+    void *stream = malloc(size);
 
     memcpy(stream, &(op_code){RESULTADO_HANDSHAKE}, sizeof(op_code));
     memcpy(stream + sizeof(op_code), &resultado, sizeof(resultado_t));
 
-    if (send(fd_conexion, stream, size, 0) <= 0) {
+    if (send(fd_conexion, stream, size, 0) <= 0)
+    {
         log_error(logger, "Error en send (fd %d)", fd_conexion);
         exit(EXIT_FAILURE);
     }
@@ -146,20 +148,24 @@ void enviar_resultado_handshake(int fd_conexion, resultado_t resultado, t_log* l
     free(stream);
 }
 
-resultado_t resultado_handshake(int fd_conexion, t_log* logger) {
+resultado_t resultado_handshake(int fd_conexion, t_log *logger)
+{
     op_code codigo_operacion;
     resultado_t resultado;
-    if (recv(fd_conexion, &codigo_operacion, sizeof(op_code), MSG_WAITALL) <= 0) {
+    if (recv(fd_conexion, &codigo_operacion, sizeof(op_code), MSG_WAITALL) <= 0)
+    {
         log_error(logger, "Error en recv (fd %d)", fd_conexion);
         exit(EXIT_FAILURE);
     }
 
-    if (codigo_operacion != RESULTADO_HANDSHAKE) {
+    if (codigo_operacion != RESULTADO_HANDSHAKE)
+    {
         log_error(logger, "No se pudo recibir el resultado del handshake");
         exit(EXIT_FAILURE);
     }
 
-    if (recv(fd_conexion, &resultado, sizeof(resultado_t), MSG_WAITALL) <= 0) {
+    if (recv(fd_conexion, &resultado, sizeof(resultado_t), MSG_WAITALL) <= 0)
+    {
         log_error(logger, "Error en recv (fd %d)", fd_conexion);
         exit(EXIT_FAILURE);
     }
--- worker/src/main.c
+++ worker/src/main.c
@@ -23,11 +23,7 @@ int main(int argc, char *argv[])
         log_info(logger_worker, "## handshake con master realizado");
     */
 
-    // Conexiones con Storage y Master
-
-    int storage_socket = connect_to_server(IP_STORAGE, PUERTO_STORAGE, logger_worker);
-    enviar_handshake(HANDSHAKE_WORKER_STORAGE, storage_socket, "Storage", argv[2]);
-    verificar_resultado_handshake(storage_socket, "Storage");
+    // Conexiones con Master
 
     int master_socket = connect_to_server(IP_MASTER, PUERTO_MASTER, logger_worker);
     enviar_handshake(HANDSHAKE_WORKER_MASTER, master_socket, "Master", argv[2]);
@@ -37,94 +33,94 @@ int main(int argc, char *argv[])
     init_memoria();
     t_list *instrucciones;
 
-    while (true)
-    {
-        // Espera de Query (Paqute: nombre del archivo, query ID, PC)
-        paquete_t *paquete_query = recibir_paquete(master_socket, logger_worker);
-        if (paquete_query->codigo_operacion != SOLICITUD_EJECUCION)
-        {
-            log_error(logger_worker, "Se recibio un paquete con un op_code distinto a %d", SOLICITUD_EJECUCION);
-            exit(EXIT_FAILURE);
-        }
+    // while (true)
+    // {
+    //     // Espera de Query (Paqute: nombre del archivo, query ID, PC)
+    //     paquete_t *paquete_query = recibir_paquete(master_socket, logger_worker);
+    //     if (paquete_query->codigo_operacion != SOLICITUD_EJECUCION)
+    //     {
+    //         log_error(logger_worker, "Se recibio un paquete con un op_code distinto a %d", SOLICITUD_EJECUCION);
+    //         exit(EXIT_FAILURE);
+    //     }
 
-        buffer_t *buffer = obtener_siguiente_item(paquete_query);
-        char *path = string_from_format("%s/%s", PATH_SCRIPTS, (char *){buffer->stream});
-        liverar_buffer(buffer);
+    //     buffer_t *buffer = obtener_siguiente_item(paquete_query);
+    //     char *path = string_from_format("%s/%s", PATH_SCRIPTS, (char *){buffer->stream});
+    //     liverar_buffer(buffer);
 
-        buffer = obtener_siguiente_item(paquete_query);
-        char *query_id = string_duplicate((char *){buffer->stream});
-        liverar_buffer(buffer);
+    //     buffer = obtener_siguiente_item(paquete_query);
+    //     char *query_id = string_duplicate((char *){buffer->stream});
+    //     liverar_buffer(buffer);
 
-        buffer = obtener_siguiente_item(paquete_query);
-        int pc = *(int *){buffer->stream};
-        liverar_buffer(buffer);
+    //     buffer = obtener_siguiente_item(paquete_query);
+    //     int pc = *(int *){buffer->stream};
+    //     liverar_buffer(buffer);
 
-        destruir_paquete(paquete_query);
+    //     destruir_paquete(paquete_query);
 
-        log_info(logger_worker, "## Query %s: Se recibe la Query. El path de operaciones es: %s", query_id, path);
+    //     log_info(logger_worker, "## Query %s: Se recibe la Query. El path de operaciones es: %s", query_id, path);
 
-        instrucciones = parsear_archivo(path);
-        free(path);
+    //     instrucciones = parsear_archivo(path);
+    //     free(path);
 
-        bool fin = false;
-        // Lectura de instrucciones
-        while (list_size(instrucciones) > pc && !fin)
-        {
-            t_instrucion *instruccion = list_get(instrucciones, pc);
-            log_info(logger_worker, "## Query %s: FETCH - Program Counter: %d - %s", query_id, pc, instruccion->identificador);
+    //     bool fin = false;
+    //     // Lectura de instrucciones
+    //     while (list_size(instrucciones) > pc && !fin)
+    //     {
+    //         t_instrucion *instruccion = list_get(instrucciones, pc);
+    //         log_info(logger_worker, "## Query %s: FETCH - Program Counter: %d - %s", query_id, pc, instruccion->identificador);
 
-            switch (instruccion->copi)
-            {
-            case CREATE:
-                interpretar_CREATE(instruccion, query_id, storage_socket);
-                break;
-            case COMMIT:
-                interpretar_COMMIT(instruccion, query_id, storage_socket);
-                break;
-            case DELETE:
-                interpretar_DELETE(instruccion, query_id, storage_socket);
-                break;
-            case FLUSH:
-                interpretar_FLUSH(instruccion, query_id, storage_socket);
-                break;
-            case TRUNCATE:
-                interpretar_TRUNCATE(instruccion, query_id, storage_socket);
-                break;
-            case TAG:
-                interpretar_TAG(instruccion, query_id, storage_socket);
-                break;
-            case READ:
-                interpretar_READ(instruccion, query_id, storage_socket, master_socket);
-                break;
-            case WRITE:
-                interpretar_WRITE(instruccion, query_id, storage_socket);
-                break;
-            default: // END
-                interpretar_END(instruccion, query_id, master_socket);
-                fin = true; // Sale del while y elimina la query
-                break;
-            }
+    //         switch (instruccion->copi)
+    //         {
+    //         case CREATE:
+    //             interpretar_CREATE(instruccion, query_id, storage_socket);
+    //             break;
+    //         case COMMIT:
+    //             interpretar_COMMIT(instruccion, query_id, storage_socket);
+    //             break;
+    //         case DELETE:
+    //             interpretar_DELETE(instruccion, query_id, storage_socket);
+    //             break;
+    //         case FLUSH:
+    //             interpretar_FLUSH(instruccion, query_id, storage_socket);
+    //             break;
+    //         case TRUNCATE:
+    //             interpretar_TRUNCATE(instruccion, query_id, storage_socket);
+    //             break;
+    //         case TAG:
+    //             interpretar_TAG(instruccion, query_id, storage_socket);
+    //             break;
+    //         case READ:
+    //             interpretar_READ(instruccion, query_id, storage_socket, master_socket);
+    //             break;
+    //         case WRITE:
+    //             interpretar_WRITE(instruccion, query_id, storage_socket);
+    //             break;
+    //         default: // END
+    //             interpretar_END(instruccion, query_id, master_socket);
+    //             fin = true; // Sale del while y elimina la query
+    //             break;
+    //         }
 
-            // Idea para manejar interrupciones desde el worker
-            send(master_socket, &(op_code){CONSULTA_INTERRUPCION}, sizeof(int), 0); // Podria ser un paquete
-            bool resultado;
-            recv(master_socket, &resultado, sizeof(bool), MSG_WAITALL); // True: es necesario interrumpir la ejecucion, False: se continua con normalidad
-            if (resultado)
-            {
-                break; // sale del while y espera un nuevo query (Aqui se puede agregar un paquete si es necesario para el master)
-            }
+    //         // Idea para manejar interrupciones desde el worker
+    //         send(master_socket, &(op_code){CONSULTA_INTERRUPCION}, sizeof(int), 0); // Podria ser un paquete
+    //         bool resultado;
+    //         recv(master_socket, &resultado, sizeof(bool), MSG_WAITALL); // True: es necesario interrumpir la ejecucion, False: se continua con normalidad
+    //         if (resultado)
+    //         {
+    //             break; // sale del while y espera un nuevo query (Aqui se puede agregar un paquete si es necesario para el master)
+    //         }
 
-            pc++;
-        }
+    //         pc++;
+    //     }
 
-        list_destroy_and_destroy_elements(instrucciones, destruir_instrucciones);
-    }
+    //     list_destroy_and_destroy_elements(instrucciones, destruir_instrucciones);
+    // }
 
-    // Liveracion de Recursos
-    config_destroy(config_worker);
-    log_destroy(logger_worker);
-    // list_destroy_and_destroy_elements(tabla_paginas, destruir_paginas); TODO
-    free(memoria);
+    // // Liveracion de Recursos
+    // config_destroy(config_worker);
+    // log_destroy(logger_worker);
+    // // list_destroy_and_destroy_elements(tabla_paginas, destruir_paginas); TODO
+    // free(memoria);
 
     return 0;
 }
